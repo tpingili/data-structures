0887d98bdb1e4d4e0f333727b512177b9e30c620
Tejaswini Pingili <tpingili@users.noreply.github.com>
2014-07-23 16:12:40 -0700

    Implemented stack and queue for the functional pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,28 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
+  var front = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[front+size] = value;
+    size++;
   };
 
   someInstance.dequeue = function(){
+    if(size > 0){
+      var result = storage[front];
+      delete storage[front];
+      front++;
+      size--;
+      return result;
+    }
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
