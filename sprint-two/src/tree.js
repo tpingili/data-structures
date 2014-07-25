60b50ea31c630a4814e75575bc4347cd40653d6c
tpingili <ptejaswini.reddy@gmail.com>
2014-07-25 12:08:53 -0700

    Implemented basic requirements for Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,24 +1,33 @@
 var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
-  newTree.children = undefined;
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
   return newTree;
 };
-
-
-
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children.push(makeTree(value))
 };
 
 treeMethods.contains = function(target){
-
+  if(this.value === target){
+    return true;
+  }else if(this.children.length){
+    for(var i = 0; i < this.children.length; i++){
+      if(this.children[i].contains(target)){
+        return true;
+      }
+    }
+  }
+  return false;
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * .addChild(value) = O(1)
+ * .contains(target) = O(n)
  */
