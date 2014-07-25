c82cab0ee7edc27c216c0a498e0c5e86c6869dcd
tpingili <ptejaswini.reddy@gmail.com>
2014-07-24 20:09:54 -0700

    Implemented basic requirements for LinkedList.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,40 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = makeNode(value);
+    if(list.head === null){
+      list.head = list.tail = newNode;
+    }else{
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function(){
+    if(list.head !== null){
+      var result = list.head.value;
+      if(list.head.next !== null){
+        list.head = list.head.next;
+      }else{
+        list.head = list.tail = null;
+      }
+      return result;
+    }
   };
 
   list.contains = function(target){
+    debugger;
+    if(list.head!== null){
+      var currentNode = list.head;
+      while(currentNode!== null){
+        if(currentNode.value === target){
+          return true;
+        }else {
+          currentNode = currentNode.next;
+        }
+      }
+    }
+    return false;
   };
 
   return list;
