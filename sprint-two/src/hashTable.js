14807dc1b1be5bf5f1e200a2c1cdea473ed1189d
tpingili <ptejaswini.reddy@gmail.com>
2014-07-25 22:04:00 -0700

    Working on Hash Tables with linked-list buckets to deal with collisions.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,13 +5,50 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+
+  var firstNode = this._storage.get(i);
+
+  var createNode = function(key, value){
+    return {'key':key, 'value':value, 'next':null};
+  };
+
+  var insertAt = function(currentNode, key, value){
+    if(currentNode.next === null){
+      currentNode.next = createNode(key, value);
+    }else{
+      insertAt(currentNode.next, key, value);
+    }
+  };
+
+  if(!firstNode){
+    this._storage.set(i,createNode(k,v));
+  }else{
+    insertAt(firstNode);
+  }
   //we're going to store v in ._storage at index i
-  this._storage.set(i, v);
+  //this._storage.set(i, v);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(i);
+
+  var findAt = function(currentNode, key){
+    if(currentNode){
+      if(currentNode.key === k){
+        return currentNode.value;
+    }else if(currentNode.next){
+      return findAt(currentNode.next, k);
+      }
+    }else{
+      return null;
+    }
+  };
+
+  var firstNode = this._storage.get(i);
+  //var foundNode = null;
+  var foundValue = findAt(firstNode, k);
+  return foundValue;
+  //return this._storage.get(i).value;
 };
 
 HashTable.prototype.remove = function(k){
